3487ab15f856f5881c42a837ba39a68d446805b4
iagprog <lgsoftlab@gmail.com>
2019-10-07 07:14:09 +0300

    lesson6-7 homework completed

---
+++ lesson6-7/task1.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS homework;
+CREATE DATABASE homework;
+USE homework;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+ 
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
+) COMMENT = 'Заказы';
+
+INSERT INTO orders
+(user_id)
+VALUES (1),(3),(5); -- id пользователей, которые сделали заказ
+
+SELECT
+	name, id AS user_id
+FROM
+	users
+WHERE 
+	id IN (SELECT user_id FROM orders); -- выбрали пользователей по id из orders
+	
